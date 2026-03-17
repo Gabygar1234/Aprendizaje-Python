98ea3a8aca4b67d181265af5a339c697bfc297cb
Gabriel Garcia <gabrielgar079@gmail.com>
2026-03-17 11:46:26 -0300

    Archivo de practica - curso Python

---
+++ cursopyt_sublime_practicas.py
@@ -0,0 +1,18 @@
+# Combinacion de Tuplas con Diccionarios
+
+# Definición de una Tupla con nombres de clubes
+Tupla_2 = ("Milan", "Juventus", "River", "Chelsea")
+
+# Creación de un Diccionario usando los índices de la tupla como llaves
+Dicciona_tupl = {
+    Tupla_2[0]: "Gattuso", 
+    Tupla_2[1]: "Nedved", 
+    Tupla_2[2]: "Mora", 
+    Tupla_2[3]: "Drogba"
+}
+
+print(Dicciona_tupl)
+
+
+
+
